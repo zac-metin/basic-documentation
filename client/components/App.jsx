0b51a039339d6c7539ea75be10cf3186dffcb8c1
alan-jordan <alanpjordan@gmail.com>
2017-06-27 13:07:26 +1200

    documentation template working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
-import Greetings from './Greetings'
+import Tester from './Tester'
 
 const App = () => (
   <Router>
     <div className='app-container'>
       <h1>Hello</h1>
-      <Route exact path="/" component={Greetings} />
+      <Route exact path="/" component={Tester} />
     </div>
   </Router>
 )
---
+++ client/components/Tester.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Tester = () => (
+  <div></div> )
+
+
+module.exports= Tester
