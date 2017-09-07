49f784df488958b8cb907e749581ef00792af27f
zac-metin <zac.metin@gmail.com>
2017-09-07 13:39:39 +1200

    add jest mocks

--- client/components/Links.jsx
+++ client/components/Links.jsx
@@ -7,7 +7,8 @@ const Links = () => (
     <a href="#nockpost" id="link">Nock - Post</a>
     <a href="#enzymeshallow" id="link">Enzyme - Shallow</a>
     <a href="#enzymemount" id="link">Enzyme - Mount</a>
-    <a href="#jesttest" id="link">Jest</a>
+    <a href="#jesttest" id="link">Jest Basics</a>
+    <a href="#jestmock" id="link">Jest Mocks</a>
     <a href="#reduxflow" id="link">Redux Flow</a>
   </div>
 )
